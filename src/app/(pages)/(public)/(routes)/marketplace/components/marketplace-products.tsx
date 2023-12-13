91af863c7d821cd1596088c1a3f108c88ca217e1
Asalu <toptemi285@gmail.com>
2023-12-13 19:15:31 +0100

    marketplace pagination

--- src/app/(pages)/(public)/(routes)/marketplace/components/marketplace-products.tsx
+++ src/app/(pages)/(public)/(routes)/marketplace/components/marketplace-products.tsx
@@ -1,11 +1,22 @@
 import {Product} from '@/types/types';
 import MarketPlaceProductCard from '@/components/cards/marketplace-product-card';
+import { useGlobalStore } from '@/hooks/use-global-store';
+import { Button } from '@/components/ui/button';
+import { RotateCw } from 'lucide-react';
+import { Dispatch, SetStateAction } from 'react';
 
 interface MarketplaceProductsProps {
-	products: Product[];
+	//products: Product[];
+	// id: number;
+	// title: string;
+	// value: string;
+	currentPage: number;
+	updateCurrentPage: Dispatch<SetStateAction<number>>;
 }
 
-const MarketplaceProducts = ({products}: MarketplaceProductsProps) => {
+const MarketplaceProducts = ({currentPage, updateCurrentPage}: MarketplaceProductsProps) => {
+	const {products, totalPages, hasNextPage} = useGlobalStore();
+	
 	return (
 		<div className='flex flex-col w-full bg-white px-4 md:px-8'>
 			<div className='flex flex-wrap items-center w-full justify-start gap-y-10 gap-x-2 sm:gap-x-5 md:gap-x-10 mt-10'>
@@ -16,6 +27,36 @@ const MarketplaceProducts = ({products}: MarketplaceProductsProps) => {
 					/>
 				))}
 			</div>
+			{!hasNextPage && totalPages > 1 && (
+				<div className='flex justify-center mt-10'>
+					<Button
+						type='button'
+						variant={'outline'}
+						onClick={() => {
+							updateCurrentPage(1);
+						}}
+						className='flex items-center space-x-1 bg-white border hover:bg:white focus:bg-white'
+					>
+						<RotateCw />
+						<span>Reset</span>
+					</Button>
+				</div>
+			)}
+			{hasNextPage && (
+				<div className='flex justify-center mt-10'>
+					<Button
+						type='button'
+						variant={'outline'}
+						onClick={() => {
+							updateCurrentPage(currentPage + 1);
+						}}
+						className='flex items-center space-x-1 bg-white border hover:bg:white focus:bg-white'
+					>
+						<RotateCw />
+						<span>Load More</span>
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/app/(pages)/(public)/(routes)/sellers/[sellerId]/components/seller-info-products.tsx
+++ src/app/(pages)/(public)/(routes)/sellers/[sellerId]/components/seller-info-products.tsx
@@ -1,10 +1,18 @@
 'use client';
 import {useGlobalStore} from '@/hooks/use-global-store';
 import SellerProductCard from '@/components/cards/seller-product-card';
+import { Button } from '@/components/ui/button';
+import { RotateCw } from 'lucide-react';
+import { Dispatch, SetStateAction } from 'react';
 
 
-const SellerInfoProducts = () => {
-	const {products} = useGlobalStore();
+interface SellerInfoProductsProps {
+	currentPage: number;
+	updateCurrentPage: Dispatch<SetStateAction<number>>;
+}
+
+const SellerInfoProducts = ({currentPage, updateCurrentPage}: SellerInfoProductsProps) => {
+	const {products, totalPages, hasNextPage} = useGlobalStore();
 
 	return (
 		<div className='flex flex-col w-full bg-white '>
@@ -13,6 +21,36 @@ const SellerInfoProducts = () => {
 					<SellerProductCard key={product.id} product={product} />
 				))}
 			</div>
+			{!hasNextPage && totalPages > 1 && (
+				<div className='flex justify-center mt-10'>
+					<Button
+						type='button'
+						variant={'outline'}
+						onClick={() => {
+							updateCurrentPage(1);
+						}}
+						className='flex items-center space-x-1 bg-white border hover:bg:white focus:bg-white'
+					>
+						<RotateCw />
+						<span>Reset</span>
+					</Button>
+				</div>
+			)}
+			{hasNextPage && (
+				<div className='flex justify-center mt-10'>
+					<Button
+						type='button'
+						variant={'outline'}
+						onClick={() => {
+							updateCurrentPage(currentPage + 1);
+						}}
+						className='flex items-center space-x-1 bg-white border hover:bg:white focus:bg-white'
+					>
+						<RotateCw />
+						<span>Load More</span>
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };

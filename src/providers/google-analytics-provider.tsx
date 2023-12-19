5c68edd5c877849d36f245c131ea631016032415
tisanyada <tisanyada@gmail.com>
2023-12-19 12:20:07 +0100

    updated google analytics config[9]

--- src/providers/google-analytics-provider.tsx
+++ src/providers/google-analytics-provider.tsx
@@ -1,10 +1,49 @@
-import ReactGA from 'react-ga';
+'use client';
+import {useEffect} from 'react';
+import Script from 'next/script';
+import {pageview} from '@/lib/gtagHelper';
+import {usePathname, useSearchParams} from 'next/navigation';
 
-const useGoogleAnalyticsEventTracker = (category: string) => {
-	const eventTracker = (action = 'test action', label = 'test label') => {
-		ReactGA.event({category, action, label});
-	};
-	return eventTracker;
-};
+export default function GoogleAnalyticsProvider() {
+	const pathName = usePathname();
+	const searchParams = useSearchParams();
 
-export default useGoogleAnalyticsEventTracker;
+	useEffect(() => {
+		const url = pathName + searchParams.toString();
+
+		pageview(url);
+	}, [pathName, searchParams]);
+
+	return (
+		<>
+			{' '}
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+			/>
+			<Script
+				id='google-analytics'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            
+                            gtag('consent', 'default', {
+                                'analytics_storage': 'denied'
+                            })
+
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
+				}}
+			/>
+		</>
+	);
+}
+
+// gtag('consent', 'update', {
+//     'analytics_storage': 'granted'
+// })

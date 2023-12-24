797cb5dbc584d12f7ec954f6159ea7cbc03a3e3a
tisanyada <tisanyada@gmail.com>
2023-12-24 09:49:39 +0100

    updated search form UI | API setup

--- src/app/(pages)/(public)/(routes)/components/search-form.tsx
+++ src/app/(pages)/(public)/(routes)/components/search-form.tsx
@@ -7,9 +7,12 @@ import {useGlobalStore} from '@/hooks/use-global-store';
 import ButtonLoader from '@/components/loader/button-loader';
 import FormTextInput from '@/components/input/form-text-input';
 import ProductCard from '@/components/cards/product-card';
+import {NigeriaStates} from '@/data';
+import {usePathname} from 'next/navigation';
 
 type FormData = {
 	query: string;
+	location: string;
 };
 
 type FormAction = {
@@ -19,6 +22,7 @@ type FormAction = {
 
 const initialState: FormData = {
 	query: '',
+	location: '',
 };
 
 const formReducer = (state: FormData, action: FormAction) => {
@@ -31,12 +35,10 @@ const formReducer = (state: FormData, action: FormAction) => {
 };
 
 const SearchForm = () => {
-	const {
-		searchProducts,
-		updateProducts,
-		updatePagination,
-		updateSearchProducts,
-	} = useGlobalStore();
+	const pathName = usePathname();
+
+	const {searchProducts, updateVendors, updateProducts, updatePagination} =
+		useGlobalStore();
 
 	const [loading, setLoading] = useState<boolean>(false);
 	const [formData, updateFormData] = useReducer(formReducer, initialState);
@@ -48,31 +50,48 @@ const SearchForm = () => {
 				query: event.target.value,
 			},
 		});
+	};
 
-		if (event.target.value === '' || event.target.value.length <= 2) {
-			return;
-		} else {
-			setTimeout(() => {
-				handleSearch();
-			}, 800);
-		}
+	const handleSelectChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		updateFormData({
+			type: 'UPDATE_FORMDATA',
+			payload: {
+				location: event.target.value,
+			},
+		});
 	};
 
 	const handleSearch = async () => {
 		try {
 			setLoading(true);
 
-			const {data} = await axios.get(
-				`${process.env.NEXT_PUBLIC_API_URL}/user/products/search?query=${formData.query}`
-			);
+			if (!pathName.includes('sellers')) {
+				const {data} = await axios.get(
+					`${process.env.NEXT_PUBLIC_API_URL}/user/products/search?query=${formData.query}&location=${formData.location}`
+				);
 
-			// console.log('[DATA] ::  ', data);
-			// updateSearchProducts(data.data.products);
+				// console.log('[DATA] ::  ', data);
+				// updateSearchProducts(data.data.products);
 
-			updateProducts(data.data.products);
-			updatePagination(data.data.totalPages, data.data.hasNext);
+				updateProducts(data.data.products);
+				updatePagination(data.data.totalPages, data.data.hasNext);
 
-			setLoading(false);
+				setLoading(false);
+			} else {
+				const {data} = await axios.get(
+					`${process.env.NEXT_PUBLIC_API_URL}/user/search-sellers?query=${formData.query}&location=${formData.location}`
+				);
+
+				// console.log('[DATA] ::  ', data);
+				// updateSearchProducts(data.data.vendors);
+
+				updateVendors(data.data.vendors);
+				updatePagination(data.data.totalPages, data.data.hasNext);
+
+				setLoading(false);
+			}
 		} catch (error) {
 			setLoading(false);
 
@@ -99,50 +118,69 @@ const SearchForm = () => {
 		}
 	};
 
+	const fetchSellers = async () => {
+		try {
+			const {data} = await axios.get(
+				`${process.env.NEXT_PUBLIC_API_URL}/user/sellers/fetch-all?page=1`
+			);
+
+			// console.log('[DATA] ::  ', data);
+
+			updateVendors(data.data.vendors);
+			updatePagination(data.data.totalPages, data.data.hasNext);
+		} catch (error) {
+			const _error = error as AxiosError;
+
+			console.log('[SEARCH-PRODUCTS-ERROR] :: ', _error);
+		}
+	};
+
 	useEffect(() => {
-		if (formData.query === '') {
-			// updateSearchProducts([]);
+		if (formData.query === '' && !pathName.includes('sellers')) {
 			fetchProducts();
+		}
+		if (formData.query === '' && pathName.includes('sellers')) {
+			fetchSellers();
 		}
 	}, [formData.query]);
 
+	useEffect(() => {
+		if (formData.query.length > 2 || formData.location) {
+			handleSearch();
+		}
+	}, [formData.query, formData.location]);
+
 	return (
 		<div className='w-full'>
-			<div className='flex w-full justify-center items-center gap-x-5 relative'>
+			<div className='flex flex-col md:flex-row w-full justify-center items-center gap-x-5 relative'>
 				<FormTextInput
 					name='query'
 					value={formData.query}
 					handleChange={handleChange}
 					placeHolder='What are you looking for?'
-					classes='w-[70%] sm:w-[60%] bg-[#00A65120] text-white text-sm placeholder:text-sm placeholder:italic border-2 border-green-600 focus:border-green-600 rounded-full'
+					classes='w-[85%] md:w-[60%] bg-[#00A65120] text-white text-sm placeholder:text-sm placeholder:italic border-2 border-green-600 focus:border-green-600 rounded-full'
 				/>
 
-				{/* {searchProducts?.length > 0 && (
-					<div className="absolute z-[5] w-full top-14 mx-auto bg-[#11111180] backdrop-blur-sm h-screen">
-					<div className='z-[6] mx-auto bg-white max-h-[650px] w-full sm:w-[50%] shadow-md border border-t-0 overflow-y-auto scrollbar__1 flex flex-wrap justify-evenly gap-y-2 gap-x-2 pt-4 pb-14 px-4 md:px-0'>
-						{searchProducts?.map((product) => (
-							<ProductCard key={product.id} product={product} />
+				<div className='w-[85%] mt-4 md:mt-0 md:w-fit'>
+					<select
+						name='location'
+						className='w-full py-4 rounded-full bg-main px-3 text-sm text-white outline-none scrollbar__1'
+						onChange={handleSelectChange}
+					>
+						<option value=''>
+							{formData.location ? formData.location : 'Location'}
+						</option>
+						{NigeriaStates.map((option) => (
+							<option
+								key={option}
+								value={option}
+								className='cursor-pointer'
+							>
+								{option}
+							</option>
 						))}
-					</div>
-					</div>
-				)} */}
-
-				{loading ? (
-					<Button
-						type='button'
-						className='bg-main hover:bg-main rounded-full w-12 h-12'
-					>
-						<ButtonLoader />
-					</Button>
-				) : (
-					<Button
-						type='button'
-						onClick={handleSearch}
-						className='bg-main hover:bg-main rounded-full w-12 h-12'
-					>
-						<Search className='h-12 w-12 text-white' />
-					</Button>
-				)}
+					</select>
+				</div>
 			</div>
 		</div>
 	);

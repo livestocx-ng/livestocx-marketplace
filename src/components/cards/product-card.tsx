bac663415f2c68df12397dadcecf209cf0b185a3
tisanyada <tisanyada@gmail.com>
2024-01-22 19:47:45 +0100

    updated product card UI[3]

--- src/components/cards/product-card.tsx
+++ src/components/cards/product-card.tsx
@@ -12,7 +12,7 @@ import {usePathname, useRouter} from 'next/navigation';
 import {PriceFormatter} from '@/utils/price.formatter';
 import {
 	Forward,
-	MapPin,
+	Navigation,
 	ShoppingCartIcon,
 	ThumbsDown,
 	ThumbsUp,
@@ -212,8 +212,8 @@ const ProductCard = ({product}: ProductCardProps) => {
 
 				{product?.vendor?.state && (
 					<div className='border-t border-slate-400 text-xs font-medium px-2 pt-1 flex items-center space-x-2'>
-						<MapPin className='h-4 w-4 text-black' />
-						<p className='text-xs'>{product?.vendor?.state}</p>
+						<Navigation className='h-4 w-4 text-black' />
+						<p className='text-xs'>{product?.vendor?.state === 'Federal Capital Territory' ? 'Abuja' : product?.vendor?.state}</p>
 					</div>
 				)}
 			</div>

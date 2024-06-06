5b4f5a8c468110807667482983cfd3711105ef32
tisanyada <tisanyada@gmail.com>
2024-06-06 21:51:40 +0100

    updated home screen loading animation to loading card skeleton[3]

---
+++ src/components/skeletons/product-card-skeleton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProductCardSkeleton = () => {
+	return (
+		<div className='w-[48%] sm:w-[150px] flex flex-col justify-between shadow__1 relative'>
+			<div className='h-[100px] bg-slate-300 rounded-t-md'>{/* IMAGE DIV */}</div>
+
+			<div className='flex flex-col justify-between bg-slate-100 border border-t-0 border-slate-200 py-2 relative h-[100px] rounded-b-md transition-all duration-1000 animate-pulse ease-in-out'>
+				<div className='space-y-1'>
+					<div className='flex justify-between items-center px-2'>
+						<div className=' flex items-center justify-center h-8 sm:h-8 w-8 sm:w-8 bg-slate-200 rounded-full'>
+							{/* PRODUCT LIKE ICON */}
+						</div>
+
+						<div className=' flex items-center justify-center h-8 sm:h-8 w-8 sm:w-8 bg-slate-200 rounded-full'>
+							{/* MESSAGE SELLER ICON */}
+						</div>
+
+						<div className=' flex items-center justify-center h-8 sm:h-8 w-8 sm:w-8 bg-slate-200 rounded-full'>
+							{/* SHARE PRODUCT ICON */}
+						</div>
+					</div>
+
+					<div className='text-xs sm:text-sm font-semibold px-2'>
+						{/* PRODUCT NAME */}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProductCardSkeleton;

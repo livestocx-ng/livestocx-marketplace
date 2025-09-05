4fec10a0e6a8d10ea712248c99e20003c2629146
tisan <admin@venille.com.ng>
2025-09-05 07:33:50 +0100

    updated footer UI setup

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import {motion} from 'framer-motion';
 import {useRouter} from 'next/navigation';
 import {useGlobalStore} from '@/hooks/use-global-store';
 import {AccountNavLinks, HelpNavLinks, OtherNavLinks, Sponsors} from '@/data';
@@ -11,183 +12,210 @@ const Footer = () => {
 	const {updateCurrentAccountTab} = useGlobalStore();
 
 	return (
-		<div className='pt-5 md:pt-10 w-full bg-main flex flex-col px-5 sm:px-10'>
+		<div className='pt5 md:pt10 w-full bg-main flex flex-col'>
 			<div className='flex flex-row flex-wrap items-start justify-between w-full'>
-				<div className='flex flex-col justify-between w-full mb-5 md:mb-10'>
-					<div className='flex justify-center items-center text-white text-md md:text-xl mb-5'>
+				<div className='flex flex-col justify-between w-full pt-5 md:pt-10  bg-white'>
+					<div className='flex justify-center items-center text-whte text-md md:text-xl mb-5'>
 						Backed by
 					</div>
-					<div className='flex flex-row flex-wrap items-center justify-center gap-4 w-full'>
-						{Sponsors.map((sponsor) => (
-							<Image
-								width={80}
-								height={80}
-								src={sponsor.image}
-								className='object-cover rounded-md'
-								alt={`Livestocx Backed by: ${sponsor.name}`}
-							/>
+					<div className='overflow-hidden w-full'>
+						<motion.div
+							style={{
+								display:
+									'flex flex-row flex-wrap items-center justify-center gap-4 w-full',
+								gap: 40,
+								width: 'max-content',
+							}}
+							className='flex flex-row flex-wrap items-center justify-center gap-4 w-full pb-5'
+							animate={{
+								x: [0, -1000],
+							}}
+							transition={{
+								duration: 30,
+								repeat: Infinity,
+								repeatType: 'loop',
+								ease: 'linear',
+							}}
+						>
+							{[...Sponsors, ...Sponsors].map((sponsor) => (
+								<Image
+									width={80}
+									height={80}
+									src={sponsor.image}
+									className='object-cover rounded-md'
+									alt={`Livestocx Backed by: ${sponsor.name}`}
+								/>
+							))}
+						</motion.div>
+					</div>
+				</div>
+				<div className='px-5 sm:px-10 pt-10 flex flex-row flex-wrap items-start justify-between w-full'>
+					<div className='mb-10 sm:mb-0 flex flex-col space-y-5 w-full md:w-fit'>
+						<Image
+							alt='logo'
+							width={30}
+							height={30}
+							className=''
+							src={'/logo.svg'}
+							unoptimized={true}
+						/>
+
+						<h1 className='text-white text-xs'>Livestocx</h1>
+
+						<div className='flex flex-col items-start space-y-2'>
+							<div className='space-y-1'>
+								<p className='text-white text-xs'>
+									1680 East Barstow Avenue, <br /> Fresno
+									California, United States
+								</p>
+								<p className='text-white text-xs'>
+									+1(559)6683783
+								</p>
+							</div>
+							<div className='space-y-1'>
+								<p className='text-white text-xs'>
+									Shimex Estate Lugbe Federal Capital
+									Territory, Nigeria.
+								</p>
+								<p className='text-white text-xs'>
+									+2348132549273
+								</p>
+							</div>
+
+							<p className='text-white text-xs'>
+								support@livestocx.com
+							</p>
+						</div>
+					</div>
+
+					<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
+						<h1 className='text-white text-sm'>My Account</h1>
+
+						{AccountNavLinks.map((link) => (
+							<p
+								key={link.title}
+								onClick={() => {
+									router.push(link.url);
+
+									if (link.currentTab) {
+										updateCurrentAccountTab(
+											link?.currentTab
+										);
+									}
+								}}
+								className='text-white text-xs cursor-pointer'
+							>
+								{link.title}
+							</p>
 						))}
 					</div>
-				</div>
-				<div className='mb-10 sm:mb-0 flex flex-col space-y-5 w-full md:w-fit'>
-					<Image
-						alt='logo'
-						width={30}
-						height={30}
-						className=''
-						src={'/logo.svg'}
-						unoptimized={true}
-					/>
 
-					<h1 className='text-white text-xs'>Livestocx</h1>
+					<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
+						<h1 className='text-white text-sm'>Helps</h1>
 
-					<div className='flex flex-col items-start space-y-2'>
-						<div className='space-y-1'>
-							<p className='text-white text-xs'>
-								1680 East Barstow Avenue, <br /> Fresno
-								California, United States
-							</p>
-							<p className='text-white text-xs'>+1(559)6683783</p>
-						</div>
-						<div className='space-y-1'>
-							<p className='text-white text-xs'>
-								Shimex Estate Lugbe Federal Capital Territory,
-								Nigeria.
-							</p>
-							<p className='text-white text-xs'>+2348132549273</p>
-						</div>
-
-						<p className='text-white text-xs'>
-							support@livestocx.com
-						</p>
+						{HelpNavLinks.map((link) => (
+							<Link
+								key={link.title}
+								href={link.url}
+								className='text-white text-xs'
+							>
+								{link.title}
+							</Link>
+						))}
 					</div>
-				</div>
 
-				<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
-					<h1 className='text-white text-sm'>My Account</h1>
+					<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
+						<h1 className='text-white text-sm'>Other</h1>
 
-					{AccountNavLinks.map((link) => (
-						<p
-							key={link.title}
-							onClick={() => {
-								router.push(link.url);
+						{OtherNavLinks.map((link) => (
+							<Link
+								key={link.title}
+								href={link.url}
+								className='text-white text-xs'
+							>
+								{link.title}
+							</Link>
+						))}
+					</div>
 
-								if (link.currentTab) {
-									updateCurrentAccountTab(link?.currentTab);
+					<div className='flex flex-col justify-start md:items-center md:justify-center space-y-5 mt-5'>
+						<div className='flex items-center space-x-10 sm:space-x-3'>
+							<Link
+								target='_blank'
+								href={
+									'https://www.facebook.com/profile.php?id=61550763699954'
 								}
-							}}
-							className='text-white text-xs cursor-pointer'
-						>
-							{link.title}
-						</p>
-					))}
-				</div>
-
-				<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
-					<h1 className='text-white text-sm'>Helps</h1>
-
-					{HelpNavLinks.map((link) => (
-						<Link
-							key={link.title}
-							href={link.url}
-							className='text-white text-xs'
-						>
-							{link.title}
-						</Link>
-					))}
-				</div>
-
-				<div className='mb-5 md:mb-10 sm:mb-0 flex flex-col space-y-5'>
-					<h1 className='text-white text-sm'>Other</h1>
-
-					{OtherNavLinks.map((link) => (
-						<Link
-							key={link.title}
-							href={link.url}
-							className='text-white text-xs'
-						>
-							{link.title}
-						</Link>
-					))}
-				</div>
-
-				<div className='flex flex-col justify-start md:items-center md:justify-center space-y-5 mt-5'>
-					<div className='flex items-center space-x-10 sm:space-x-3'>
-						<Link
-							target='_blank'
-							href={
-								'https://www.facebook.com/profile.php?id=61550763699954'
-							}
-						>
-							<Image
-								alt={''}
-								width={30}
-								height={30}
-								className='object-cover'
-								unoptimized={true}
-								src={'/icon__facebook.svg'}
-							/>
-						</Link>
-						<Link
-							className=''
-							target='_blank'
-							href={
-								'https://www.linkedin.com/company/livestocxltd/'
-							}
-						>
-							<Image
-								alt={''}
-								width={30}
-								height={30}
-								unoptimized={true}
-								className='object-cover'
-								src={'/icon__linkedin.svg'}
-							/>
-						</Link>
-						<Link
-							target='_blank'
-							href={
-								'https://instagram.com/livestocx?igshid=MzNlNGNkZWQ4Mg=='
-							}
-						>
-							<Image
-								alt={''}
-								width={30}
-								height={30}
-								unoptimized={true}
-								className='object-cover'
-								src={'/icon__instagram.svg'}
-							/>
-						</Link>
-					</div>
-					<div className='flex items-center space-x-3'>
-						<Link href={'#'} className=''>
-							<Image
-								alt={''}
-								width={120}
-								height={60}
-								unoptimized={true}
-								className='object-cover'
-								src={'/icon__appstore__2.svg'}
-							/>
-						</Link>
-						<Link
-							className=''
-							target='_blank'
-							// href={'#'}
-							href={
-								'https://play.google.com/store/apps/details?id=com.livestocx.livestocx_mobile&pcampaignid=web_share'
-							}
-						>
-							<Image
-								alt={''}
-								width={120}
-								height={60}
-								unoptimized={true}
-								className='object-cover'
-								src={'/icon__playstore__2.svg'}
-							/>
-						</Link>
+							>
+								<Image
+									alt={''}
+									width={30}
+									height={30}
+									className='object-cover'
+									unoptimized={true}
+									src={'/icon__facebook.svg'}
+								/>
+							</Link>
+							<Link
+								className=''
+								target='_blank'
+								href={
+									'https://www.linkedin.com/company/livestocxltd/'
+								}
+							>
+								<Image
+									alt={''}
+									width={30}
+									height={30}
+									unoptimized={true}
+									className='object-cover'
+									src={'/icon__linkedin.svg'}
+								/>
+							</Link>
+							<Link
+								target='_blank'
+								href={
+									'https://instagram.com/livestocx?igshid=MzNlNGNkZWQ4Mg=='
+								}
+							>
+								<Image
+									alt={''}
+									width={30}
+									height={30}
+									unoptimized={true}
+									className='object-cover'
+									src={'/icon__instagram.svg'}
+								/>
+							</Link>
+						</div>
+						<div className='flex items-center space-x-3'>
+							<Link href={'#'} className=''>
+								<Image
+									alt={''}
+									width={120}
+									height={60}
+									unoptimized={true}
+									className='object-cover'
+									src={'/icon__appstore__2.svg'}
+								/>
+							</Link>
+							<Link
+								className=''
+								target='_blank'
+								// href={'#'}
+								href={
+									'https://play.google.com/store/apps/details?id=com.livestocx.livestocx_mobile&pcampaignid=web_share'
+								}
+							>
+								<Image
+									alt={''}
+									width={120}
+									height={60}
+									unoptimized={true}
+									className='object-cover'
+									src={'/icon__playstore__2.svg'}
+								/>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
